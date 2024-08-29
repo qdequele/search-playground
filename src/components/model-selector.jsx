8916010478f9b0c2d3b01579b2b140d1aa0384d2
qdequele <quentin@dequelen.me>
2024-08-29 17:01:18 +0200

    change mode selector

--- src/components/model-selector.jsx
+++ src/components/model-selector.jsx
@@ -119,7 +119,7 @@ export function ModelSelector({ className, onChange, defaultValue }) {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[300px] p-0" align="start">
         <Command>
           <CommandInput placeholder="Search model..." />
           <CommandList>
